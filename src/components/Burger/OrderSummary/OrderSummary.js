a93b7e5238ceea6c5ddc2f0c1e7d825f73fc4d45
preeti1 <preeti.z747@gmail.com>
2020-05-05 21:54:47 +0530

    demo project

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Aux';
+
+const orderSummary=(props)=>{
+    const ingredientSummary=Object.keys(props.ingredients)
+    .map(igKey => {
+        return (
+        <li key={igKey}>
+            <span style={{textTransform:'capitalize'}}>{igKey}</span>
+            :{props.ingredients[igKey]}
+            </li>);
+    });
+    return(
+        <Aux>
+            <h3>your order</h3>
+            <p>Yipee..your delicious burger is ready with the following ingredients: </p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+            <button>CANCEL</button>
+            <button>CONTINUE</button>
+        </Aux>
+    );
+};
+
+export default orderSummary;
